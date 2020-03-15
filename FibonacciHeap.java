f529ace3ac68af67da716d0fd841b37cfe14f4ed
Rajat Rai <rajatrai1206@gmail.com>
2020-03-14 22:30:00 -0400

    Commit 3

--- FibonacciHeap.java
+++ FibonacciHeap.java
@@ -1,56 +1,59 @@
+/*
+* @authon rajatrai101
+*/
+
 import java.io.File;
+import java.util.List;
+import java.util.Queue;
+import java.util.HashMap;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Queue;
 
 class Node {
 	int deg = 0;
-	boolean childCutVal = false;
+	int data;
 	Node next;
 	Node prev;
-	Node parent;
 	Node child;
-	int data;
 	String key;
+	Node parent;
+	boolean childCutVal = false;
+
 	/**
 	 * Constructs a new Node with the data/value specified by the elem
-	 *
-	 * @param elem
-	 *            The data/hash tag count stored in this node.
-	 * @param keyVal
-	 *            The keyVal is the value of the string associated with this node.
+	 * 
+	 * @param keyValue The keyVal is the value of the string associated with this
+	 *                 node.
+	 * @param count    The hash tag count stored in this node.
 	 */
-	Node(int elem, String keyVal) {
+	Node(String keyValue, int count) {
 		next = prev = this;
-		data = elem;
-		key = keyVal;
+		data = count;
+		key = keyValue;
 	}
 }
 
-class FibonacciHeap {
+class fibonacciHeap {
 	Queue<Node> queueA = new LinkedList<Node>();
 	/* Pointer to the maximum element in the heap. */
 	Node max = null;
-	HashMap<String, Node> hashMap;
-	/* Heap size is maintained explicitly to avoid recomputation.*/
+	HashMap<String, Node> hashMap = new HashMap<String, Node>();
+	/* Heap size is maintained explicitly to avoid recomputation. */
 	int size = 0;
 
 	/**
-	 *Inserts a node into the Fibonacci heap with the specified value and key
+	 * Inserts a node into the Fibonacci heap with the specified value and key
 	 *
-	 * @param value
-	 *            The value to insert.
-	 * @param key
-	 *            The associated hashtag/key
+	 * @param key   The associated hashtag/key
+	 * @param value The value to insert.
 	 * @return An Node representing that element in the heap.
 	 */
-	public Node insert(int value, String key) {
-		Node result = new Node(value, key);
+	public Node insert(String key, int value) {
+		Node result = new Node(key, value);
 
 		/* Merge this singleton list with the tree list. */
 		max = combineLists(max, result);
@@ -60,6 +63,7 @@ class FibonacciHeap {
 
 		return result;
 	}
+
 	/**
 	 * Returns whether the heap is empty.
 	 *
@@ -68,12 +72,12 @@ class FibonacciHeap {
 	public boolean isEmpty() {
 		return max == null;
 	}
-	
+
 	/**
 	 * Returns the max value of the heap
+	 * 
 	 * @return The largest element of the heap.
-	 * @throws NoSuchElementException
-	 *             If the heap is empty.
+	 * @throws NoSuchElementException If the heap is empty.
 	 */
 	public Node max() {
 		if (isEmpty())
@@ -82,34 +86,34 @@ class FibonacciHeap {
 	}
 
 	/**
-	 * Dequeues and returns the maximum element of the Fibonacci heap. If the
-	 * heap is empty, this throws a NoSuchElementException.
+	 * Dequeues and returns the maximum element of the Fibonacci heap. If the heap
+	 * is empty, this throws a NoSuchElementException.
 	 *
 	 * @return The largest element of the Fibonacci heap.
-	 * @throws NoSuchElementException
-	 *             If the heap is empty.
+	 * @throws NoSuchElementException If the heap is empty.
 	 */
 	public Node removeMax() {
-		if (isEmpty()){
+		if (isEmpty()) {
 			throw new NoSuchElementException("Heap is empty.");
 		}
 		--size;
-		/*Maximum element that is to be removed*/
+		/* Maximum element that is to be removed */
 		Node maxElem = max;
 
-		/*Remove the max elem from the root list
-		 * if max elem is the only elem in the root list, make max null
-		 * else, arbitrarily reassign max to the node next to max
-		 * */
-		if (max.next == max) { 
+		/*
+		 * Remove the max elem from the root list if max elem is the only elem in the
+		 * root list, make max null else, arbitrarily reassign max to the node next to
+		 * max
+		 */
+		if (max.next == max) {
 			max = null;
-		} else { 
+		} else {
 			max.prev.next = max.next;
 			max.next.prev = max.prev;
 			max = max.next;
 		}
 
-		/*Reassign the parent field of the children of the removed max to null*/
+		/* Reassign the parent field of the children of the removed max to null */
 		if (maxElem.child != null) {
 			Node curr = maxElem.child;
 			do {
@@ -118,16 +122,19 @@ class FibonacciHeap {
 			} while (curr != maxElem.child);
 		}
 
-		/*move the children of the old max to the root list*/
+		/* move the children of the old max to the root list */
 		max = combineLists(max, maxElem.child);
 
 		if (max == null)// If the list becomes empty, return the max and we are done
 			return maxElem;
 
-		/*Treetable that keeps track of  the dgree of the subtrees during pairwise combine*/
+		/*
+		 * Treetable that keeps track of the dgree of the subtrees during pairwise
+		 * combine
+		 */
 		List<Node> treeTab = new ArrayList<Node>();
 
-		/*List of nodes to visit during the traversal*/
+		/* List of nodes to visit during the traversal */
 		List<Node> nodeToVisit = new ArrayList<Node>();
 
 		for (Node curr = max; nodeToVisit.isEmpty() || nodeToVisit.get(0) != curr; curr = curr.next)
@@ -135,11 +142,11 @@ class FibonacciHeap {
 
 		for (Node curr : nodeToVisit) {
 			while (true) {
-				while (curr.deg >= treeTab.size()){
+				while (curr.deg >= treeTab.size()) {
 					treeTab.add(null);
 				}
 
-				/*Keep traversing until two trees of the same degree are found*/
+				/* Keep traversing until two trees of the same degree are found */
 				if (treeTab.get(curr.deg) == null) {
 					treeTab.set(curr.deg, curr);
 					break;
@@ -151,11 +158,11 @@ class FibonacciHeap {
 				Node min = (other.data < curr.data) ? other : curr;
 				Node max = (other.data < curr.data) ? curr : other;
 
-				/*Remove the minimum element from the list*/
+				/* Remove the minimum element from the list */
 				min.next.prev = min.prev;
 				min.prev.next = min.next;
 
-				/*Make the min child of max by pairwise combine*/
+				/* Make the min child of max by pairwise combine */
 				min.next = min.prev = min;
 				max.child = combineLists(max.child, min);
 				min.parent = max;
@@ -164,8 +171,8 @@ class FibonacciHeap {
 				++max.deg;
 				curr = max;
 			}
-			//Update the max element
-			if (curr.data >= max.data){
+			// Update the max element
+			if (curr.data >= max.data) {
 				max = curr;
 			}
 		}
@@ -175,33 +182,53 @@ class FibonacciHeap {
 	/**
 	 * Increases the key of a node by the specified value
 	 *
-	 * @param heapNode
-	 *            The element whose data should be increased.
-	 * @param addVal
-	 *            The value by which the Node's value needs to be increased
+	 * @param heapNode The element whose data should be increased.
+	 * @param addVal   The value by which the Node's value needs to be increased
 	 */
 	public void increaseKey(Node heapNode, int addVal) {
-		/* Increase the node's value by addVal*/
+		/* Increase the node's value by addVal */
 		heapNode.data += addVal;
 
-		/*If the node's new value is greater than that of it's parent then cut the node and insert it into the root list*/
-		if (heapNode.parent != null && heapNode.data >= heapNode.parent.data){
+		/*
+		 * If the node's new value is greater than that of it's parent then cut the node
+		 * and insert it into the root list
+		 */
+		if (heapNode.parent != null && heapNode.data >= heapNode.parent.data) {
 			childCut(heapNode);
 		}
 
-		/*If the new value of the node is greater than the max node's value then the max node is pointed to the current node with the increased value*/
-		if (heapNode.data >= max.data){
+		/*
+		 * If the new value of the node is greater than the max node's value then the
+		 * max node is pointed to the current node with the increased value
+		 */
+		if (heapNode.data >= max.data) {
 			max = heapNode;
 		}
 	}
 
 	/**
+     * Deletes this Node from the Fibonacci heap that contains it.
+     *
+     * It is assumed that the heapNode belongs in this heap if it is present in the hashmap/fibbonacci heap
+     *
+     * @param heapNode The entry to delete.
+     */
+    public void deleteKey(Node heapNode) {
+        /* Use increaseKey to drop the entry's key to infinity.  This will
+		 * guarantee that the node is cut and set to the global maximum.
+		 */
+		if(hashMap.containsKey(heapNode.key))
+        	increaseKey(heapNode, Integer.MAX_VALUE);
+
+        /* Call removeMax to remove it. */
+        removeMax();
+    }
+
+	/**
 	 * Merges two doubly linked lists in O(1)time
 	 * 
-	 * @param p
-	 *            A pointer into p of the q linked lists.
-	 * @param q
-	 *            A pointer into the other of the q linked lists.
+	 * @param p A pointer into p of the q linked lists.
+	 * @param q A pointer into the other of the q linked lists.
 	 * @return A pointer to the smallest element of the resulting list.
 	 */
 	private static Node combineLists(Node p, Node q) {
@@ -212,7 +239,7 @@ class FibonacciHeap {
 		} else if (p == null && q != null) { // p is null, result is q.
 			return q;
 		} else { // combine the lists if both p and q is non null
-			Node pNext = p.next; 
+			Node pNext = p.next;
 			p.next = q.next;
 			p.next.prev = p;
 			q.next = pNext;
@@ -222,14 +249,15 @@ class FibonacciHeap {
 			return p.data > q.data ? p : q;
 		}
 	}
-	
+
 	/**
 	 * Removes the first N max elements from the Fibonacci heap.
-	 * @param n
-	 *            The number of maxes to remove
+	 * 
+	 * @param n The number of maxes to remove
+	 * @throws IOException
 	 */
-	public void removeNMaxes(int n) throws Exception {
-		/*The output is written to the output_file.txt*/
+	public void printNMaxes(int n) throws IOException {
+		/* The output is written to the output_file.txt */
 		FileWriter writer = new FileWriter(new File("output_file.txt"), true);
 		PrintWriter pw = new PrintWriter(writer);
 		for (int i = 0; i < n; i++) {
@@ -247,17 +275,15 @@ class FibonacciHeap {
 		pw.close();
 		while (!queueA.isEmpty()) {
 			Node ins = (Node) queueA.remove();
-			Node res = insert(ins.data, ins.key);
+			Node res = insert(ins.key, ins.data);
 			hashMap.get(ins.key).next = res;
 		}
 	}
 
-
 	/**
 	 * Recursively cuts the marked parents of a node
 	 *
-	 * @param heapNode
-	 *            The node to cut from its parent.
+	 * @param heapNode The node to cut from its parent.
 	 */
 	private void childCut(Node heapNode) {
 		heapNode.childCutVal = false;
@@ -265,28 +291,31 @@ class FibonacciHeap {
 		if (heapNode.parent == null)
 			return;
 
-		/*remove the node from it's siblings list*/
-		if (heapNode.next != heapNode) { 
+		/* remove the node from it's siblings list */
+		if (heapNode.next != heapNode) {
 			heapNode.next.prev = heapNode.prev;
 			heapNode.prev.next = heapNode.next;
 		}
 
-		/*Change  the child pointer of the parent of the cut node, if necessary*/
+		/* Change the child pointer of the parent of the cut node, if necessary */
 		if (heapNode.parent.child == heapNode) {
 			if (heapNode.next != heapNode) {
 				heapNode.parent.child = heapNode.next;
-			}else {
+			} else {
 				heapNode.parent.child = null;
 			}
 		}
 
 		--heapNode.parent.deg;
 
-		/*Add the cut node to the root list*/
+		/* Add the cut node to the root list */
 		heapNode.prev = heapNode.next = heapNode;
 		max = combineLists(max, heapNode);
 
-		/*Recursively cut the parents if marked already else mark their childCut to true*/
+		/*
+		 * Recursively cut the parents if marked already else mark their childCut to
+		 * true
+		 */
 		if (heapNode.parent.childCutVal)
 			childCut(heapNode.parent);
 		else
--- hashtagcounter.java
+++ hashtagcounter.java
@@ -4,10 +4,11 @@
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.io.FileNotFoundException;
 
 /**
  * Hashtagcounter class containing the main method the reads the file specified
@@ -15,48 +16,60 @@ import java.util.HashMap;
  *
  */
 public class hashtagcounter {
-	public static void main(String[] args) throws Exception {
-		/* Read from the file got from the argument */
+	public static void main(String[] args) {
+		/*
+		 * Try read from the file got from the argument try-catch block to handle any
+		 * errors
+		 */
+		fibonacciHeap f = new fibonacciHeap();
 		try {
 			FileWriter writer = new FileWriter(new File("output_file.txt"));
 			writer.write("");
 			writer.flush();
 			writer.close();
-			
-			FibonacciHeap f = new FibonacciHeap();
-			f.hashMap = new HashMap<String, Node>();
-			String str;
-			
-			/* Read from the file got from the argument*/
+
+			String nextLine;
+
+			/* Read from the file got from the argument */
 			File filename = new File(args[0]);
 			FileInputStream fstream = new FileInputStream(filename);
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fstream));
-	
-			while ((str = bufferedReader.readLine()) != null && !str.equals("stop")) {
-				String[] strArr = str.split(" ");
-				if (str.indexOf('#') != -1) {
+
+			while ((nextLine = bufferedReader.readLine()) != null && !nextLine.equals("stop")) {
+				String[] strArr = nextLine.split(" ");
+				if (nextLine.indexOf('#') != -1) {
 					strArr[0] = strArr[0].substring(1);
-					int val = Integer.parseInt(strArr[1]);
+					int count = Integer.parseInt(strArr[1]);
 					if (f.hashMap.containsKey(strArr[0])) {
-						/*increase the value of the key if the node is already present*/
-						f.increaseKey(f.hashMap.get(strArr[0]).next, val);
+						/* increase the count of the hashtag if it is already present */
+						f.increaseKey(f.hashMap.get(strArr[0]).next, count);
 					} else {
-						/*insert the new node if the key is not present already*/
-						Node ins = f.insert(val, strArr[0]);
-						Node pointerNode = new Node(-1, null);
+						/* insert the new hashtag if it is not present already */
+						Node ins = f.insert(strArr[0], count);
+						Node pointerNode = new Node(null, -1);
 						pointerNode.next = ins;
 						f.hashMap.put(strArr[0], pointerNode);
 					}
 				} else {
 					// call remove max method with the remNum times
 					Integer remNum = Integer.parseInt(strArr[0]);
-					f.removeNMaxes(remNum);
+					try {
+						f.printNMaxes(remNum);
+					} catch (IOException e) {
+						System.err.printf("No such file exiests: " + e.getMessage());
+					}
 				}
 			}
 			bufferedReader.close();
 		} catch (ArrayIndexOutOfBoundsException e) {
-			// Error handling code here.
+			// Error handling if no filename specified
 			System.err.printf("Input file not provided. Arguments array size:" + e.getMessage());
+		} catch (FileNotFoundException e) {
+			// Error handling if filename did not match any files
+			System.err.printf("No such file exiests: " + e.getMessage());
+		} catch (IOException e) {
+			// Error handling if unable the output file
+			System.err.printf("Error initializing output file." + e.getMessage());
 		}
 	}
 
